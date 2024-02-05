798819e4ee5c65e7c806f78201c1d4356b0e2fc3
Fernando <fernandocatalamunyoz@gmail.com>
2024-02-05 18:29:38 +0100

    Añado pantalla de inicio y bloqueo canales si la television esta encendida

--- js/main.js
+++ js/main.js
@@ -1,12 +1,53 @@
-const buttons = document.getElementsByClassName("numberButtons");
+let onOff = document.getElementById("buttonOnOff");
+let screenTv = document.getElementById("screenTv");
+let tvStatus = false;
 
+const buttons = document.getElementsByClassName("numberButtons");
 let arrayButtons = Array.from(buttons);
 
 let screen = document.getElementById("screenTv");
 
-arrayButtons.map((channel) => {
-  channel.addEventListener("click", (e) => {
-    screen.classList.remove(screen.classList);
-    screen.classList.add("channel" + e.target.id.slice(-1));
-  });
+//evento de hacer click en el boton de encendido
+onOff.addEventListener("click", () => {
+  tvStatus = !tvStatus;
+  console.log(tvStatus);
+
+  if (tvStatus) {
+    screenTv.classList.add("startChannel");
+  } else {
+    screenTv.classList.remove(
+      screenTv.classList[screenTv.classList.length - 1]
+    );
+  }
 });
+//Evento para cmabiar de canal si la tele esta encendida o no
+if (!tvStatus) {
+  arrayButtons.map((channel) => {
+    channel.addEventListener("click", (e) => {
+      if (tvStatus) {
+        screenTv.classList.remove(
+          screenTv.classList[screenTv.classList.length - 1]
+        );
+        screenTv.classList.add("channel" + e.target.id.slice(-1));
+      }
+    });
+  });
+}
+
+let hour = document.getElementById("hourDate");
+setInterval(function () {
+  let data = new Date();
+  return (hour.innerHTML =
+    data.getDate() +
+    "/" +
+    data.getMonth() +
+    1 +
+    "/" +
+    data.getFullYear() +
+    "  " +
+    data.toLocaleTimeString());
+});
+
+setTimeout(function () {
+  return (hour.style.display = "hidden ");
+}, 3);
