71e41ff5ea4cf8d5f4506fe36f8f2a01490f410b
Fernando <fernandocatalamunyoz@gmail.com>
2024-02-06 17:29:16 +0100

    Anañdop imagenes y GIFS a los canales y doy formato de colores al mando

---
+++ README.md
@@ -0,0 +1,59 @@
+# Carta Restaurante 🖋️
+
+Bienvenido a RAKUTEN TV. Aqui podras disfrutar de los mejores canales de entretenimiento.
+
+## Tabla de contenidos🗂️
+
+- [Tecnologias 🛠️](#tecnologías)
+- [Características 🌟](#caracteristicas)
+- [Imptlementación 📡](#implementación)
+- [Instalación 🚀](#instalación)
+- [Capturas :pager:](#capturas-de-pantalla)
+- [Bugs :cry:](#bugs)
+- [Autor ✒️](#autor)
+- [Agradecimientos 🎓](#agradecimientos)
+
+## Tecnologías
+
+![Html](./img/html-5.png){width=30} ![CSS3](./img/css-3.png){width=30} ![CSS3](./img/bootstrap.svg){width=30} ![JAVASCRIPT](../TV%20interactiva/img/js.png){width=30}
+
+## Caracteristicas
+
+- **Video presentacion**
+- **Menu:** Presentación de los distintos platos disponibles.
+- **Sobre nosotros:** Conoce un poco mas sobre nosotros.
+- **Reserva:** Posibilidad de realizar una reserva a traves de un formulario.
+
+Utilizacion de Bootstrap para conseguir que sea responsive en cualquier dispositivo.
+
+## Implementación
+
+Pruebalo en Github Pages https://fernandocatalamunyoz.github.io/Carta_Restaurante/
+
+## Instalación
+
+Haz una copia del proyecto y arrancalo en tu ordenador para desarrollo y pruebas.
+
+```
+git clone https://github.com/FernandoCatalaMunyoz/Carta_Restaurante.git
+```
+
+[def]: ./img/html-5.png
+
+## Capturas de pantalla
+
+![CapturaInicio](./img/captura%20inicio.png){width=200} ![CarturaMenu](./img/caputra%20menu.png){width=200} ![CapturaAbout](./img/captura%20sobre%20nosotros.png){width=200} ![CapturaContacto](./img/camptura%20contacto.png){width=200}
+
+## Bugs
+
+- no he sabido centrar del todo bien las opciones del navegador cuando se convierte en version movil
+
+## Autor
+
+- **Fernando Catalá Muñoz** - Desarrollador
+- **Github** https://github.com/FernandoCatalaMunyoz
+- **LinkedIn** linkedin.com/in/fernando-catalá-muñoz-166b5622b
+
+## Agradecimientos
+
+- A **Geekshubs Academy** por la formacion necesaria para este proyecto
--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 let onOff = document.getElementById("buttonOnOff");
 let screenTv = document.getElementById("screenTv");
+//declaro variable del estado de la tele
 let tvStatus = false;
 
-const buttons = document.getElementsByClassName("numberButtons");
-let arrayButtons = Array.from(buttons);
-
 let screen = document.getElementById("screenTv");
+
+let infoDate = document.getElementById("dateHour");
+let infoChannel = document.getElementById("channelName");
+infoDate.style.display = "none";
+infoChannel.style.display = "none";
 
 //evento de hacer click en el boton de encendido
 onOff.addEventListener("click", () => {
   tvStatus = !tvStatus;
   console.log(tvStatus);
+  infoDate.style.display = "none";
 
   if (tvStatus) {
     screenTv.classList.add("startChannel");
@@ -18,36 +22,66 @@ onOff.addEventListener("click", () => {
     screenTv.classList.remove(
       screenTv.classList[screenTv.classList.length - 1]
     );
+    hideInfoChannel();
+    hideInfoDate();
   }
 });
-//Evento para cmabiar de canal si la tele esta encendida o no
+
+//Evento para cambiar de canal si la tele esta encendida o no
+
+const buttons = document.getElementsByClassName("numberButtons");
+let arrayButtons = Array.from(buttons);
 if (!tvStatus) {
   arrayButtons.map((channel) => {
     channel.addEventListener("click", (e) => {
+      infoDate.style.display = "block";
+      infoChannel.style.display = "block";
+
+      let channelName = document.getElementById("channelName");
+      // console.log(channelName);
+      console.log(e.target.id);
+      document.getElementById("channelName").innerHTML = e.target.id;
+
+      setTimeout(hideInfoDate, 3000);
+      setTimeout(hideInfoChannel, 3000);
+
       if (tvStatus) {
+        showInfo(Date);
+
         screenTv.classList.remove(
           screenTv.classList[screenTv.classList.length - 1]
         );
         screenTv.classList.add("channel" + e.target.id.slice(-1));
+      } else {
+        infoDate.style.display = "none";
+        infoChannel.style.display = "none";
       }
     });
   });
 }
+//Funcion para mostrar la info de fecha
+function showInfo() {
+  let hour = document.getElementById("dateHour");
+  setInterval(function () {
+    let data = new Date();
+    return (hour.innerHTML =
+      data.getDate() +
+      "/" +
+      data.getMonth() +
+      1 +
+      "/" +
+      data.getFullYear() +
+      "  " +
+      data.toLocaleTimeString());
+  });
+}
+//Función para ocultar la informacion de fecha y hora
+function hideInfoDate() {
+  infoDate.style.display = "none";
+}
+//Funcion para ocultar el nombre del canal
+function hideInfoChannel() {
+  return (infoChannel.style.display = "none");
+}
 
-let hour = document.getElementById("hourDate");
-setInterval(function () {
-  let data = new Date();
-  return (hour.innerHTML =
-    data.getDate() +
-    "/" +
-    data.getMonth() +
-    1 +
-    "/" +
-    data.getFullYear() +
-    "  " +
-    data.toLocaleTimeString());
-});
-
-setTimeout(function () {
-  return (hour.style.display = "hidden ");
-}, 3);
+//Funcion para mostrar el nombre del canal
